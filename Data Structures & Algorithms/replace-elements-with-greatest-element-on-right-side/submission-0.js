900dbdb4bc63561cd208bd03bcb4b0b9f58d0317
Obhi <10656131+obhi1kenobhi@users.noreply.github.com>
2026-05-04 12:30:52 +0530

    Add: replace-elements-with-greatest-element-on-right-side - submission-0

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @return {number[]}
+     */
+    replaceElements(arr) {
+        
+        for(let i = 0; i < arr.length; i++){
+            let greatest = 0
+            for(let j = i+1; j < arr.length; j++){
+                if(arr[j] > greatest){
+                    greatest = arr[j]
+                }
+            }
+            arr[i] = greatest
+        }
+        arr[arr.length-1] = -1
+        return arr
+    }
+}
